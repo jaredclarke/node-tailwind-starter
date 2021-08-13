20909ff968a00c7c5246bae40f68eeff151a948a
nyctonio <nyctonio.dev@gmail.com>
2021-08-13 12:56:07 +0530

    node-express-tailwind-starter

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyparser=require("body-parser");
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.json());
+app.use(express.static("public"));
+
+
+app.get('/',(req,res)=>{
+    res.render('home');
+})
+
+
+
+app.listen(port,()=>{
+    console.log(`Running on port ${port}`);
+})
